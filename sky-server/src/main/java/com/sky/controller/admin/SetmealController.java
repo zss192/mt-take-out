733a3bf7b59ddde365be4a892f931b55b52340ec
zss192 <zhaoshishun1@gmail.com>
2024-03-21 17:48:14 +0800

    feat: ✨ cache setmeal

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -2,15 +2,13 @@ package com.sky.controller.admin;
 
 import com.sky.dto.SetmealDTO;
 import com.sky.dto.SetmealPageQueryDTO;
-import com.sky.entity.Setmeal;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.SetmealService;
 import com.sky.vo.SetmealVO;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Update;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,8 +22,10 @@ import java.util.List;
 public class SetmealController {
     @Autowired
     private SetmealService setmealService;
+
     /**
      * 根据id查询套餐
+     *
      * @param id
      * @return
      */
@@ -37,56 +37,65 @@ public class SetmealController {
 
     /**
      * 套餐分页查询
+     *
      * @param setmealPageQueryDTO
      * @return
      */
     @GetMapping("/page")
-    public Result<PageResult> page(SetmealPageQueryDTO setmealPageQueryDTO){
+    public Result<PageResult> page(SetmealPageQueryDTO setmealPageQueryDTO) {
         PageResult pageResult = setmealService.pageQuery(setmealPageQueryDTO);
         return Result.success(pageResult);
     }
 
     /**
      * 套餐起售、停售
+     *
      * @param status
      * @param id
      * @return
      */
     @PostMapping("/status/{status}")
-    public Result status(@PathVariable Integer status,Long id){
-        setmealService.startOrStop(status,id);
+    @CacheEvict(cacheNames = "setmealCache", allEntries = true)
+    public Result status(@PathVariable Integer status, Long id) {
+        setmealService.startOrStop(status, id);
         return Result.success();
     }
 
     /**
      * 新增套餐
+     *
      * @param setmealDTO
      * @return
      */
     @PostMapping
-    public Result add(@RequestBody SetmealDTO setmealDTO){
+    @CacheEvict(cacheNames = "setmealCache", key = "#setmealDTO.categoryId")
+    public Result add(@RequestBody SetmealDTO setmealDTO) {
         setmealService.add(setmealDTO);
         return Result.success();
     }
 
     /**
      * 修改套餐
+     *
      * @param setmealDTO
      * @return
      */
     @PutMapping
-    public Result update(@RequestBody SetmealDTO setmealDTO){
+    @CacheEvict(cacheNames = "setmealCache", allEntries = true)
+    public Result update(@RequestBody SetmealDTO setmealDTO) {
         setmealService.update(setmealDTO);
         return Result.success();
     }
 
     /**
      * 批量删除套餐
+     *
      * @param ids
      * @return
      */
     @DeleteMapping
-    public Result delete(@RequestParam List<Long> ids){
+    @CacheEvict(cacheNames = "setmealCache", allEntries = true)
+    public Result delete(@RequestParam List<Long> ids) {
         setmealService.deleteBacth(ids);
         return Result.success();
     }
